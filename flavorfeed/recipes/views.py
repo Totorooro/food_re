c6c35a4222794eb725aa43356c4d6d104ca7dacc
cleimns <urcleimns@gmail.com>
2025-09-11 16:57:17 +0300

    feat: add filtering

--- flavorfeed/recipes/views.py
+++ flavorfeed/recipes/views.py
@@ -1,15 +1,23 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views import View
 from django.views.generic import ListView
-from .models import Dish
-
+from .models import Dish, Category
 
 class RecipesView(ListView):
     template_name = 'recipes/index.html'
     context_object_name = 'dishes'
 
-
-
     def get_queryset(self):
-        return Dish.objects.all()
+        queryset = Dish.objects.all()
+        category_slug = self.request.GET.get('category')
+        difficulty = self.request.GET.get('difficulty')
+
+        if category_slug and category_slug != 'all':
+            queryset = queryset.filter(cat__slug=category_slug)
+        if difficulty and difficulty != 'all':
+            queryset = queryset.filter(difficulty=difficulty)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
