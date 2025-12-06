431c357db55ff5272733122794efc165250b2108
cleimns <urcleimns@gmail.com>
2025-12-06 18:33:29 +0300

    feat(auth): configure SMTP email delivery

--- flavorfeed/flavorfeed/settings.py
+++ flavorfeed/flavorfeed/settings.py
@@ -146,4 +146,15 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST =  os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+
+DEFAULT_FROM_EMAIL = f"{os.getenv('EMAIL_FROM_NAME')} <{os.getenv('EMAIL_FROM_ADDRESS')}>"
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
+EMAIL_SUBJECT_PREFIX = '[FlavorFeed] '
